1ee3dec337ea2268822ec7739542555e90e684e3
Hadar YACobovitz <hadaryacov@gmail.com>
2015-11-25 23:17:09 -0500

    animation and sunset

--- js/script.js
+++ js/script.js
@@ -1,40 +1,20 @@
-animateItems = function animateItems($items,animationName,callback){
-
-	$items.each(function(i,el) {
-	    var $this = $(this);
-	    setTimeout(function() {
-	    	$this.removeClass('hide');
-            $this.addClass(animationName);
-
-            appendTextToCode("$this.addClass(animationName);");
-
-            if( i === $items.length-1 && callback && typeof(callback) === "function"){
-            	callback();
-            }
-
-		}, i * 200);
-	});	
-};
-
-arrangeFlowers = function(){
-	var $lis = $('.sun .project');
-	var $li;
-	for (var i=0; i <= $lis.length - 1; i++){
-		$li = $($lis[i]);
-		var angle = (180/($lis.length-1) * i) * (Math.PI/180) ;
-		var x = 80 * Math.cos(angle) + 45;
-		var y = 80 * Math.sin(angle) + 50;
-
-		$li.css({
-			'bottom': y + 'px', 
-			'right' : x + 'px',
-			'transform' : 'rotate(' + (180/($lis.length-1) * i) + 'deg)'
-		});
-	}
-}
-
-var $body;
-
 $(document).ready(function(){
+
+	var $sky = $('.sky');
+	var $sun = $('.sun');
+	var $sunset = $('.sunset');
+	var $clouds = $('.cloud');
+	var posY = $sky.offset().top - 300;
+
+	$( window ).scroll(function() {
+		if($(window).scrollTop() > posY){
+			var per = ($(window).scrollTop() - posY)/$sky.height();
+			console.log('per',per);
+			$sun.css('top',per*100 + '%');
+			$sunset.css('opacity',per);
+			$clouds.css('opacity', 1-per);
+
+		}
+	});
 
 });
